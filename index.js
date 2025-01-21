e41ecb4cb83059f2df37e79d00340145c67429dc
ShahabAli786 <shahabmanagi0@gmail.com>
2025-01-21 07:19:35 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+// TASK 1
+alert("Hello, World!");
+
+// TASK 2
+
+let firstName = "Shahab";
+let lastName = "Ali";
+let fullName = firstName + " " + lastName;
+alert(fullName);
+
+// TASK 3
+
+let num1 = 10;
+let num2 = 5; 
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let division = num1 / num2;
+let modulus = num1 % num2;
+
+alert("Sum: " + sum);
+alert("Difference: " + difference);
+alert("Product: " + product);
+alert("Division: " + division);
+alert("Modulus: " + modulus);
+
+// TASK 4
+
+let width = 10; 
+let height = 5;
+let area = width * height;
+alert("Area of the rectangle: " + area);
+
+// TASK 5
+
+let celsius = 25; 
+let fahrenheit = (celsius * 9/5) + 32;
+alert("Temperature in Fahrenheit: " + fahrenheit);
+
+// TASK 6
+
+let count = 10;
+alert("Initial count: " + count);
+alert("Pre-increment: " + (++count)); 
+alert("Post-increment: " + (count++)); 
+alert("Count after post-increment: " + count);
+alert("Pre-decrement: " + (--count)); 
+alert("Post-decrement: " + (count--)); 
+alert("Count after post-decrement: " + count);
+
+// TASK 7
+
+let result = ((15 - 3 * 2) / 3 + 4 ** 2) * 2 - (8 / 2) ** 2;
+alert("Result of the expression: " + result);
+
+// TASK 8
+
+let name = "YourName"; 
+let age = 25; 
+let message = `Hello, ${name}! You are ${age} years old.`;
+console.log(message);
